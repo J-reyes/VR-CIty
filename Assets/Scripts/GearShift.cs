317cbcf5a961e913bf593cfdaff100ab821f2569
Jose Reyes <vazquez.jr09@gmail.com>
2018-05-13 13:02:31 -0700

    Files

---
+++ Assets/Scripts/GearShift.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class GearShift : MonoBehaviour, ICardboardGazeResponder {
+
+	// Use this for initialization
+	void Start () {
+        SetGazeAt(false);
+	}
+    void LateUpdate() {
+        Cardboard.SDK.UpdateState();
+    }
+	
+	private void SetGazeAt(bool a_flag)
+    {
+        GetComponent<Renderer>().material.color = a_flag ? Color.green : Color.red;
+    }
+    private void HandleButton()
+    {
+        Debug.Log("The Button was clicked");
+    }
+    public void OnGazeEnter()
+    {
+        SetGazeAt(true);
+    }
+    public void OnGazeExit()
+    {
+        SetGazeAt(false);
+    }
+    public void OnGazeTrigger()
+    {
+        HandleButton();
+    }
+}
